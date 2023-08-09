1813b688c2f7e54cc8a4ef6d86ac8eb3a84f0717
Oumayma <oumayma.sghari@esprit.tn>
2023-08-09 21:28:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './component/menu/menu';
+import Card from './component/card/card';
+
+function App() {
+  return (
+    <div className="App">
+   <Menu />
+   <Card/>
+    </div>
+  );
+}
+
+export default App;
